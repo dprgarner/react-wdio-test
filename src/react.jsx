6f22b604eb1b72cc3c1755da7df623831cd71e4d
David Garner <dprgarner@gmail.com>
2016-06-19 17:42:36 +0100

    Moved functions around

--- src/react.jsx
+++ src/react.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+- CommentBox
+  - CommentList
+    - Comment
+  - CommentForm
+*/
+
+let CommentList = React.createClass({
+    render: () => (
+        <div className="commentList">
+            I am the comment list.
+        </div>  
+    ),
+});
+
+let CommentForm = React.createClass({
+    render: () => (
+        <div className="commentForm">
+            Hello! CommentForm here.
+        </div>  
+    ),
+});
+
+let CommentBox = React.createClass({
+    render: () => (
+        <div className="commentBox">
+            <h1>Comments</h1>
+            <CommentList />
+            <CommentForm />
+        </div>
+    ),
+});
+
 export default function(el) {
-    // console.log($elt)
-    // $elt.html('Hello!');
-
-    let CommentBox = React.createClass({
-        render: () => (
-            <div className="commentBox">
-                Hello, world! I am a comment box.
-            </div>
-        )
-    });
-
     ReactDOM.render(<CommentBox />, el);
 }
