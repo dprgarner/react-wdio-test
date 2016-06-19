a3e433617bfb8677f84fe48e0e909815071a2eec
David Garner <dprgarner@gmail.com>
2016-06-19 18:02:23 +0100

    Comments rendered by data array

--- src/react.jsx
+++ src/react.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Remarkable from 'remarkable';
 
+let data = [
+    {id: 1, author: "Becky", text: "This is a comment"},
+    {id: 2, author: "David", text: "This is *another* comment"},
+]
+
 let Comment = React.createClass({
     rawMarkup() {
         let md = new Remarkable();
@@ -23,10 +28,14 @@ let Comment = React.createClass({
 
 let CommentList = React.createClass({
     render() {
+        let commentNodes = this.props.data.map((comment) => (
+            <Comment key={comment.id} author={comment.author}>
+                {comment.text}
+            </Comment>
+        ));
         return (
             <div className="commentList">
-                <Comment author="David">This is a comment</Comment>
-                <Comment author="Becky">This is aother comment</Comment>
+                {commentNodes}
             </div>
         );
     }
@@ -47,7 +56,7 @@ let CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -55,5 +64,5 @@ let CommentBox = React.createClass({
 });
 
 export default function(el) {
-    ReactDOM.render(<CommentBox />, el);
+    ReactDOM.render(<CommentBox data={data} />, el);
 }
