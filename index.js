1dddea562d5f0d56acedc641291e81081744e60c
David Garner <dprgarner@gmail.com>
2016-06-19 13:38:31 +0100

    ES6 is here!

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var express = require('express');
+import express from 'express';
 var app = express();
 
 app.use(express.static('public'));
 
-var bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
+
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 app.post('/api', urlEncodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
     console.log(req.body);
-    res.send('Welcome ' + req.body.username);
+    res.send(`Welcome {$req.body.username}`);
 });
 
 var port = 9000;
 app.listen(port, function () {
-    console.log('App listening on port ' + port);
+    console.log(`App listening on port ${port}`);
 });
