d4427e78777f0605ca7e8f54eb871cf78a5d05a4
David Garner <dprgarner@gmail.com>
2016-06-19 17:49:56 +0100

    Fixed context by removing arrow functions

--- src/react.jsx
+++ src/react.jsx
@@ -8,30 +8,50 @@ import ReactDOM from 'react-dom';
   - CommentForm
 */
 
+let Comment = React.createClass({
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
 let CommentList = React.createClass({
-    render: () => (
-        <div className="commentList">
-            I am the comment list.
-        </div>  
-    ),
+    render() {
+        return (
+            <div className="commentList">
+                <Comment author="David">This is a comment</Comment>
+                <Comment author="Becky">This is aother comment</Comment>
+            </div>  
+        );
+    }
 });
 
 let CommentForm = React.createClass({
-    render: () => (
-        <div className="commentForm">
-            Hello! CommentForm here.
-        </div>  
-    ),
+    render() {
+        return (
+            <div className="commentForm">
+                Hello! CommentForm here.
+            </div>  
+        );
+    }
 });
 
 let CommentBox = React.createClass({
-    render: () => (
-        <div className="commentBox">
-            <h1>Comments</h1>
-            <CommentList />
-            <CommentForm />
-        </div>
-    ),
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
 });
 
 export default function(el) {
