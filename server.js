543783fef0717e5e08ef83cb66c917a679a92748
David Garner <dprgarner@gmail.com>
2016-06-19 16:54:14 +0100

    indenting and reordering

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ let app = express();
 const COMMENTS_FILE = path.join(__dirname, 'comments.json');
 const PORT = 80;
 
-function checkFileExists(filePath) {
-    return fs.access(filePath, fs.F_OK).catch(function (err) {
-        if (err.code !== 'ENOENT') throw err;
-        return fs.writeFile(COMMENTS_FILE, '[]', 'utf8');
-    });
-}
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,38 +20,46 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Creates empty comments file if it doesn't exist
+function checkFileExists(filePath) {
+    return fs.access(filePath, fs.F_OK).catch(function (err) {
+        if (err.code !== 'ENOENT') throw err;
+        return fs.writeFile(COMMENTS_FILE, '[]', 'utf8');
+    });
+}
+
 app.get('/api/comments', function (req, res) {
     checkFileExists(COMMENTS_FILE)
-    .then(() => fs.readFile(COMMENTS_FILE))
-    .then(function (data) {
-        res.json(JSON.parse(data));
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1);
-    });
+        .then(() => fs.readFile(COMMENTS_FILE))
+        .then(function (data) {
+            res.json(JSON.parse(data));
+        })
+        .catch(function (err) {
+            console.error(err);
+            process.exit(1);
+        });
 });
 
 app.post('/api/comments', function (req, res) {
     let comments;
     checkFileExists(COMMENTS_FILE)
-    .then(() => fs.readFile(COMMENTS_FILE))
-    .then(function (data) {
-        comments = JSON.parse(data);
-        comments.push({
-            id: Date.now(),
-            author: req.body.author,
-            text: req.body.text,
+        .then(() => fs.readFile(COMMENTS_FILE))
+        .then(function (data) {
+            comments = JSON.parse(data);
+            comments.push({
+                id: Date.now(),
+                author: req.body.author,
+                text: req.body.text,
+            });
+            return fs.writeFile(COMMENTS_FILE, JSON.stringify(comments));
+        })
+        .then(function () {
+            res.json(comments);
+        })
+        .catch(function (err) {
+            console.error(err);
+            process.exit(1);
         });
-        return fs.writeFile(COMMENTS_FILE, JSON.stringify(comments));
-    })
-    .then(function () {
-        res.json(comments);
-    })
-    .catch(function (err) {
-        console.error(err);
-        process.exit(1);
-    });
 });
 
 app.listen(PORT, function () {
